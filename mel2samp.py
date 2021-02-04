476687624d53e092f51ecc341539978ce4fd26a2
Paul Sajna <hello@paulsajna.com>
2021-02-03 22:05:56 -0800

    remove dumb interdependencies

--- mel2samp.py
+++ mel2samp.py
@@ -33,9 +33,7 @@ import torch.utils.data
 import sys
 from scipy.io.wavfile import read
 
-# We're using the audio processing from TacoTron2 to make sure it matches
-sys.path.insert(0, 'tacotron2')
-from tacotron2.layers import TacotronSTFT
+from stft import STFT as TacotronSTFT
 
 MAX_WAV_VALUE = 32768.0
 
